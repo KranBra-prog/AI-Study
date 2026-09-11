52e6a115fe688f2e841eca6de005b0e49db84d9d
KranBra-prog <brachtjorgegustavo@gmail.com>
2026-09-10 21:15:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def extract_text_from_pdf(pdf_file):
 def safe_gemini_call(prompt):
     try:
         response = client.models.generate_content(
-            model='gemini-2.5-flash',
+            model = genai.GenerativeModel('gemini-3.6-flash'),
             contents=prompt,
         )
         return response.text
