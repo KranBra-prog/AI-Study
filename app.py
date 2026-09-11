bc5b04e71deb6478a2bd56a64a54cb61179a3f08
KranBra-prog <brachtjorgegustavo@gmail.com>
2026-09-11 20:56:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ st.markdown("""
  /* Posicionar el micrófono a la izquierda del botón enter sin taparlo */
     iframe[title="audio_recorder_streamlit.audio_recorder"] {
         position: absolute !important;
-        bottom: 12px !important;
-        right: 62px !important;  /* Aumentado a 62px para dejar libre la flecha */
+        bottom: 20px !important;
+        right: 64px !important;  /* Aumentado a 62px para dejar libre la flecha */
         z-index: 999999 !important;
         width: 32px !important;
         height: 32px !important;
